148c68467224db96bf7166da27a5f8681b28042a
barackmaund1 <pmaundu49@gmail.co>
2020-06-14 18:07:52 +0300

    second commit

--- src/app/components/add-tutorial/add-tutorial.component.ts
+++ src/app/components/add-tutorial/add-tutorial.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TutorialService } from 'src/app/services/tutorial.service';
 @Component({
   selector: 'app-add-tutorial',
   templateUrl: './add-tutorial.component.html',
   styleUrls: ['./add-tutorial.component.css']
 })
 export class AddTutorialComponent implements OnInit {
-
-  constructor() { }
+  tutorial = {
+    title: '',
+    description: '',
+    image:'',
+    published: false,
+    created_at:''
+  };
+  submitted = false;
+  constructor(private tutorialService: TutorialService) { }
 
   ngOnInit(): void {
   }
+  saveTutorial() {
+    const data = {
+      title: this.tutorial.title,
+      description: this.tutorial.description,
+      image: this.tutorial.image,
+      created_at: this.tutorial.created_at
+    };
+    this.tutorialService.create(data)
+    .subscribe(
+      response => {
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      });
+}
 
+  newTutorial() {
+    this.submitted = false;
+    this.tutorial = {
+      title: '',
+      description: '',
+      published: false,
+      image:'',
+      created_at:''
+
+    };
+  }
 }
